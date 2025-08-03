7cf968dd85472681c46963afc2f9a7d45567b229
Alexander Nelson <alexanortega@gmail.com>
2025-08-03 15:08:50 -0700

    template grid

---
+++ chamber/scripts/discover-places.js
@@ -0,0 +1,29 @@
+export const loadCards = async () =>{
+    try {
+      const response = await fetch('./data/places.json');
+      const places = await response.json();
+
+console.log(places)
+
+      const container = document.querySelector('.card-container');
+
+      container.innerHTML = ''; // Limpia contenido previo si lo hubiera
+
+      places.forEach(place => {
+        const cardHTML = `
+          <div class="card">
+            <h2>${place.name}</h2>
+            <figure>
+              <img src="${place.image}" alt="${place.name}" loading="lazy">
+            </figure>
+            <address>${place.address}</address>
+            <p>${place.description}</p>
+            <button>${place.button}</button>
+          </div>
+        `;
+        container.innerHTML += cardHTML;
+      });
+    } catch (error) {
+      console.error('Error loading JSON data:', error);
+    }
+  }
---
+++ chamber/scripts/discover.js
@@ -0,0 +1,5 @@
+
+import { menu } from './menu.js';
+import {loadCards} from './discover-places.js'
+menu()
+loadCards()
