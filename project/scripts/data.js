5aefc65c51f7393167cc3878da935de4ad7b4679
Alexander Nelson <alexanortega@gmail.com>
2025-08-05 21:55:10 -0700

    good

--- project/scripts/data.js
+++ project/scripts/data.js
@@ -1,13 +1,39 @@
-import { renderEvents } from './ui.js';
 
-export async function loadEvents() {
-  try {
-    const res = await fetch('./data/events.json');
-    if (!res.ok) throw new Error('Could not fetch events');
-    const data = await res.json();
-    const items = data.events.slice(0, 15); // O los que haya
-    renderEvents(items);
-  } catch (err) {
-    console.error(err);
-  }
+
+export async function fetchEvents() {
+  const url = './data/data.json'
+  const response = await fetch(url);
+  if (!response.ok) throw new Error('Failed to load events');
+  const data = await response.json();
+  console.log(data.events)
+  return data.events;
+}
+
+
+export function getUpcomingEvents(events, count = 5) {
+  const today = new Date();
+  
+  return events
+    .filter(event => new Date(event.date) >= today)
+    .sort((a, b) => new Date(a.date) - new Date(b.date))
+    .slice(0, count);
+}
+
+
+
+export function renderEventCard(event) {
+  const formattedDate = new Date(event.date).toLocaleDateString(undefined, {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+  });
+
+  return `
+    <div class="event-card">
+      <h3 class="event-title">${event.title}</h3>
+      <p class="event-date"><strong>Date:</strong> ${formattedDate}</p>
+      <p class="event-location"><strong>Location:</strong> ${event.location}</p>
+      <p class="event-description">${event.description}</p>
+    </div>
+  `;
 }
--- project/scripts/main.js
+++ project/scripts/main.js
@@ -1,3 +1,14 @@
 import { setupNav } from './nav.js';
+import {fetchEvents, getUpcomingEvents, renderEventCard} from './data.js'
 
-setupNav();
+
+document.addEventListener('DOMContentLoaded',async ()=>{
+    setupNav();
+    const upcomming         = getUpcomingEvents(
+       await fetchEvents() , 6
+    )
+    const upEnvents         = document.querySelector('#event-adds') 
+    upcomming.map((event)   => {
+        upEnvents.innerHTML += renderEventCard(event)
+    })
+})
